502d12a66ce8c68026d2d3496a4f13f8844155ef
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-02 11:35:12 +0900

    1 HospitalController : id로 검색한 결과가 없을 때 처리하는 중

--- src/main/java/com/springboot/hospitalsearchapi/controller/HospitalController.java
+++ src/main/java/com/springboot/hospitalsearchapi/controller/HospitalController.java
@@ -1,10 +1,14 @@
 package com.springboot.hospitalsearchapi.controller;
 
+import com.springboot.hospitalsearchapi.HospitalSearchApiApplication;
 import com.springboot.hospitalsearchapi.dao.HospitalDao;
 import com.springboot.hospitalsearchapi.domain.Hospital;
-import com.springboot.hospitalsearchapi.parser.ReadLineContext;
+import com.springboot.hospitalsearchapi.domain.dto.HospitalRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/hospital-api")
@@ -13,20 +17,29 @@ public class HospitalController {
     public HospitalController(HospitalDao hospitalDao){
         this.hospitalDao=hospitalDao;
     }
+
     @GetMapping(value="/search/{id}")
-    public Hospital getById(@PathVariable("id") String id){
-        return hospitalDao.findById(id);
+    public ResponseEntity<Hospital> getById(@PathVariable("id") String id){
+        Hospital hospital=hospitalDao.findById(id);
+        if(hospital!=null) {
+            return ResponseEntity.ok().body(hospital);
+        }else{
+            //return ResponseEntity.
+        }
     }
+
     @DeleteMapping(value="/delete/all")
     public ResponseEntity<Integer> deleteAll(){
         return ResponseEntity
                 .ok()
                 .body(hospitalDao.deleteAll());
     }
+
     @DeleteMapping(value="/delete/{id}")
     public ResponseEntity<Integer> deleteById(@PathVariable("id") String id){
         return ResponseEntity
                 .ok()
                 .body(hospitalDao.deleteById(id));
     }
+
 }
